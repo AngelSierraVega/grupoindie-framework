5656f20e49e5afe37760881d486dc396df240d74
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2018-02-17 12:08:04 -0600

    Project structure created

---
+++ AutoloaderFramework.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * DVLP-Framework - AutoloaderFramework
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package Framework
+ *
+ * @version GI-FRMWRK.00.00 18-02-17 Empty file created.
+ */
+
+namespace GIndie\Framework;
---
+++ Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GIndie\Framework;
+
+/**
+ * DVLP-Framework - Controller
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package Framework
+ *
+ * @version GI-FRMWRK.00.00 18-02-17 Empty class created.
+ */
+class Controller
+{
+    //put your code here
+}
